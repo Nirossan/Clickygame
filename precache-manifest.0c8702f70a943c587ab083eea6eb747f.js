c63a227148f64d75fba62bf2ce87644044fd04dd
Nirossan vijayakumar <nirossanvijayakumar@Nirossans-MacBook-Air.local>
2019-06-11 18:51:56 -0400

    Updates

---
+++ precache-manifest.0c8702f70a943c587ab083eea6eb747f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d41977d177708dc611677ec74f8566",
+    "url": "/Nirossan/Clickygame/index.html"
+  },
+  {
+    "revision": "460634b58a148978ea12",
+    "url": "/Nirossan/Clickygame/static/css/main.20459e87.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Nirossan/Clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "460634b58a148978ea12",
+    "url": "/Nirossan/Clickygame/static/js/main.a4dc9844.chunk.js"
+  },
+  {
+    "revision": "16f85ba93bfca6f709cb",
+    "url": "/Nirossan/Clickygame/static/js/runtime~main.cbab992f.js"
+  }
+]);
